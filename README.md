78839d4e04ec9f19c17ad6b7ff0c632a65354a13
Aias-Feng <767980934@qq.com>
2020-05-07 00:43:51 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Aias社区
+
+##资料
+[spring文档](https://spring.io/guides)
+
+[spring Web](https://spring.io/guides/gs/serving-web-content/)
+
+[es](https://spring.io/guides)
+
+[Github deploy key](https://)
---
+++ src/test/java/com/cdx/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdx.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
